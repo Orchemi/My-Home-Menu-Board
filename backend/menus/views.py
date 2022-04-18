de4b3447730277b12480fe6c82f2a7287bb36846
Orchemi <tmdgns1126@naver.com>
2022-04-19 03:20:34 +0900

    Add: menus app views.py
    1. initial import settings of rest_framework
    2. definition of class ListMenu, DetailMenu

--- backend/menus/views.py
+++ backend/menus/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from rest_framework import generics
+from .models import Menu
+from .serializers import MenuSeriallizer
 
-# Create your views here.
+class ListMenu(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSeriallizer
+    
+
+class DetailMenu(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSeriallizer
