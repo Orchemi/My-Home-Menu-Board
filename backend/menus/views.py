f7d1d9190c6a6007f06b7fc41bdbded63fc00eae
Orchemi <tmdgns1126@naver.com>
2022-04-21 01:01:04 +0900

    Update: the sample codes to well-known codes

--- backend/menus/views.py
+++ backend/menus/views.py
@@ -1,13 +1,29 @@
-from django.shortcuts import render, redirect
-from rest_framework import generics
+from django.shortcuts import get_list_or_404, get_object_or_404, render, redirect
+# from rest_framework import generics
+from rest_framework.response import Response
 from .models import Menu
-from .serializers import MenuSerializer
-
-class ListMenu(generics.ListCreateAPIView):
-    queryset = Menu.objects.all()
-    serializer_class = MenuSerializer
+from .serializers import MenuSerializer, MenuListSerializer
+from rest_framework.decorators import api_view
+from rest_framework import status
+import json
     
-
-class DetailMenu(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Menu.objects.all()
-    serializer_class = MenuSerializer
+    
+@api_view(['GET', 'POST'])
+def menu_index(request):
+    menus = get_list_or_404(Menu)
+    if request.method=='GET':
+        serializer = MenuListSerializer(menus, many=True)
+        return Response(serializer.data)
+    
+    elif request.method=='POST':
+        print('##############POST 왔다#############')
+        print('request', request)
+        print('request.data', request.data)
+        print('request.data_json', json.dumps(request.data))
+        
+        if request.data[0].is_valid():
+            serializer = MenuSerializer(data=request.data)
+            print('##############데이터다#############', serializer.data)
+            if serializer.is_valid(raise_exception=True):
+                serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,36 +1,80 @@
-// import logo from './logo.svg';
 // import './App.css';
-import React, { Component } from "react";
+import React from "react";
+import { useState, useEffect } from "react";
 
-class App extends Component {
-	state = {
-		menus: [],
-	};
+function App() {
+	const [menus, setMenus] = useState([]);
 
-	async componentDidMount() {
+	const getMenus = async () => {
 		try {
-			const res = await fetch("http://127.0.0.1:8000/api/");
-			const menus = await res.json();
-			this.setState({
-				menus,
-			});
+			const response = await fetch("http://127.0.0.1:8000/api/menus/");
+			const json = await response.json();
+			console.log(json);
+			setMenus(json);
 		} catch (e) {
 			console.log(e);
 		}
-	}
+	};
+	useEffect(() => {
+		getMenus();
+	}, []);
 
-	render() {
-		return (
-			<div>
-				{this.state.menus.map((item) => (
-					<div key={item.id}>
-						<h1>{item.title}</h1>
-						<span>{item.description}</span>
-					</div>
-				))}
-			</div>
-		);
-	}
+	return (
+		<div>
+			{menus.map((item) => (
+				<div key={item.id}>
+					<h1>{item.title}</h1>
+					<h2>{item.score}</h2>
+				</div>
+			))}
+
+			<form action="http://127.0.0.1:8000/api/menus/" method="POST">
+				<label htmlFor="add_menu_title">메뉴명</label>
+				<input type="text" name="title" id="add_menu_title" />
+				<label htmlFor="add_menu_description">메뉴 설명</label>
+				<input type="text" name="description" id="add_menu_description" />
+				<label htmlFor="add_menu_score">호감도</label>
+				<input type="number" name="score" id="add_menu_score" />
+				<input type="submit" value="완료" />
+			</form>
+		</div>
+	);
 }
+
+// class App extends Component {
+// 	state = {
+// 		menus: [],
+// 	};
+
+// 	async componentDidMount() {
+// 		try {
+// 			const res = await fetch("http://127.0.0.1:8000/api/menus/");
+// 			const menus = await res.json();
+// 			this.setState({
+// 				menus,
+// 			});
+// 		} catch (e) {
+// 			console.log(e);
+// 		}
+// 	}
+
+// 	render() {
+// 		return (
+// 			<div>
+// 				{this.state.menus.map((item) => (
+// 					<div key={item.id}>
+// 						<h1>{item.title}</h1>
+// 						<h2>{item.score}</h2>
+// 						<span>{item.description}</span>
+// 						<span></span>
+// 					</div>
+// 				))}
+
+// 					<form action=""></form>
+
+// 			</div>
+// 		);
+// 	}
+// }
 
 export default App;
